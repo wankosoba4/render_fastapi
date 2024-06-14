f4b3ac6dccc1fb39d0d41a6f52ab0898238a1733
wankosoba4 <166776918+wankosoba4@users.noreply.github.com>
2024-06-14 12:16:46 +0900

    test: GET/でtakoyakiを返す

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
+    return {"message": "takoyaki🐙"}
+    
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
